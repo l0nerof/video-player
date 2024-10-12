131b7b447d0cb9f2fcda2c03363e5294b7c8bbed
Volodymyr Bondar <2005volka@gmail.com>
2024-10-13 00:48:36 +0300

    Innitial commit

---
+++ index.js
@@ -0,0 +1,20 @@
+const player = new Plyr("#player", {
+  youtube: {
+    // sdk: null,
+    // api: null,
+
+    controls: 0,
+    modestbranding: 1,
+    rel: 0,
+    showinfo: 0,
+    noCookie: false,
+    disablekb: 1,
+    playsinline: 1,
+    cc_load_policy: 0,
+    iv_load_policy: 3,
+    customControls: true,
+    enablejsapi: 1,
+  },
+});
+
+window.player = player;
