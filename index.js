4f83e1707767821c56c858a23658114a5e37920a
Volodymyr Bondar <2005volka@gmail.com>
2024-11-16 21:10:50 +0200

    test

--- index.js
+++ index.js
@@ -1,16 +1,1 @@
-const preventYoutubeNavigation = () => {
-  const iframe = document.querySelector(".vjs-youtube iframe");
-  if (iframe) {
-    iframe.addEventListener("load", () => {
-      const iframeDocument =
-        iframe.contentDocument || iframe.contentWindow.document;
 
-      const links = iframeDocument.querySelectorAll("a");
-      links.forEach((link) =>
-        link.addEventListener("click", (e) => e.preventDefault())
-      );
-    });
-  }
-};
-
-preventYoutubeNavigation();
