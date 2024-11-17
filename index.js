3602040a27a65c4563b8e747fdf22685ddd3b581
Volodymyr Bondar <2005volka@gmail.com>
2024-11-17 15:14:21 +0200

    changes

--- index.js
+++ index.js
@@ -1,26 +1,27 @@
-const player = new Plyr("#player", {
-  youtube: {
-    // sdk: null,
-    // api: null,
-    controls: 0,
-    modestbranding: 1,
-    rel: 0,
-    showinfo: 0,
-    noCookie: false,
-    disablekb: 1,
-    playsinline: 1,
-    cc_load_policy: 0,
-    iv_load_policy: 3,
-    customControls: true,
-    enablejsapi: 1,
-  },
-});
-window.player = player;
+// const player = new Plyr("#player", {
+//   fullscreen: { iosNative: true },
+//   youtube: {
+//     // sdk: null,
+//     // api: null,
+//     controls: 0,
+//     modestbranding: 1,
+//     rel: 0,
+//     showinfo: 0,
+//     noCookie: false,
+//     disablekb: 1,
+//     playsinline: 1,
+//     cc_load_policy: 0,
+//     iv_load_policy: 3,
+//     customControls: true,
+//     enablejsapi: 1,
+//   },
+// });
+// window.player = player;
 
-if (/iPhone|iPad|iPod|Android/i.test(navigator.userAgent)) {
-  document
-    .querySelector(".plyr__video-embed")
-    .addEventListener("click", (e) => {
-      e.preventDefault();
-    });
-}
+// if (/iPhone|iPad|iPod|Android/i.test(navigator.userAgent)) {
+//   document
+//     .querySelector(".plyr__video-embed")
+//     .addEventListener("click", (e) => {
+//       e.preventDefault();
+//     });
+// }
